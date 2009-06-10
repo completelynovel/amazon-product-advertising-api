b669cc5e5b74826796f31bdc96052443cf00aae7
Jon Gilbraith <jon@professionalnerd.com>
2009-06-10 16:24:31 +0100

    Further refactoring of the domain, now I think finally something that'll work for the implementation of the rest of the classes.

--- lib/amazon_product_advertising_api/operations/item.rb
+++ lib/amazon_product_advertising_api/operations/item.rb
@@ -1,8 +1,8 @@
 module AmazonProductAdvertisingApi
   module Operations
-    module ItemSearch
+    module Item
       
-      class Request < AmazonProductAdvertisingApi::Operations::Base::Request
+      class ItemSearch < AmazonProductAdvertisingApi::Operations::Base::Request
         
         REQUEST_PARAMETERS = :actor, :artist, :audience_rating, :author, :availability, :brand, :browse_node, :city,
                              :composer, :condition, :conductor, :director, :item_page, :keywords, :manufacturer, :maximum_price,
@@ -56,7 +56,58 @@ module AmazonProductAdvertisingApi
           end
       
       end
-    
+      
+      class ItemLookup < AmazonProductAdvertisingApi::Operations::Base::Request
+      
+        REQUEST_PARAMETERS = :condition, :id_type, :item_id, :merchant_id, :offer_page, :related_items_page, :relationship_type, :review_page,
+                             :review_sort, :search_index, :tag_page, :tags_per_page, :tag_sort, :variation_page, :response_group
+      
+        REQUEST_PARAMETERS.each do |param|
+          self.send(:attr_accessor, param)
+        end
+        
+        def initialize(item_id, region = :uk)
+          super()
+          
+          self.item_id   = item_id
+          self.operation = "ItemLookup"
+          self.region    = region
+        end
+      
+        def parse
+          self.hpricot_data.at(:Items).search(:Item).each do |element|
+            item = AmazonProductAdvertisingApi::Operations::Base::Element.new
+        
+            queue = []
+            queue << [item, element.containers]
+        
+            queue.each do |pair|
+              current_item       = pair[0]
+              current_containers = pair[1]
+          
+              current_containers.each do |container|
+                if container.containers.size == 0
+                  current_item.add_element(container.name, container.inner_html)
+                else
+                  new_item = current_item.add_element(container.name, AmazonProductAdvertisingApi::Operations::Base::Element.new)
+                  queue << [new_item, container.containers]
+                end
+              end
+            end
+            self.response.items << item
+          end
+        end
+      
+        private
+          def params
+            REQUEST_PARAMETERS.inject({}) do |parameters, parameter|
+              parameters[parameter] = eval("self.#{parameter}") unless eval("self.#{parameter}.nil?")
+              parameters
+            end
+          end
+          
+      end
+      
     end
   end
 end
