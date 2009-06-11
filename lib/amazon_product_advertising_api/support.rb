1d26b8f7c5f8edae0f03a5cfd2c472a1ac0399bb
Jon Gilbraith <jon@professionalnerd.com>
2009-06-11 15:40:54 +0100

    Support core extensions were clashing with Rails's so namespaced them.

--- lib/amazon_product_advertising_api/support.rb
+++ lib/amazon_product_advertising_api/support.rb
@@ -1,55 +1,59 @@
-class Class
-  def cattr_reader(sym)
-    class_eval(<<-EOS, __FILE__, __LINE__)
-      unless defined? @@#{sym}  # unless defined? @@hair_colors
-        @@#{sym} = nil          #   @@hair_colors = nil
-      end                       # end
-                                #
-      def self.#{sym}           # def self.hair_colors
-        @@#{sym}                #   @@hair_colors
-      end                       # end
-                                #
-      def #{sym}                # def hair_colors
-        @@#{sym}                #   @@hair_colors
-      end                       # end
-    EOS
-  end
-
-  def cattr_writer(sym)
-    class_eval(<<-EOS, __FILE__, __LINE__)
-      unless defined? @@#{sym}                       # unless defined? @@hair_colors
-        @@#{sym} = nil                               #   @@hair_colors = nil
-      end                                            # end
-                                                     #
-      def self.#{sym}=(obj)                          # def self.hair_colors=(obj)
-        @@#{sym} = obj                               #   @@hair_colors = obj
-      end                                            # end
-    EOS
-  end
-
-  def cattr_accessor(sym)
-    cattr_reader(sym)
-    cattr_writer(sym)
-  end
+module AmazonProductAdvertisingApi
   
-end
-
-class String
-
-  def camelize(first_letter_in_uppercase = true)
-    if first_letter_in_uppercase
-      self.to_s.gsub(/\/(.?)/) { "::" + $1.upcase }.gsub(/(^|_)(.)/) { $2.upcase }
-    else
-      self.first + camelize(self)[1..-1]
+  class Class
+    def cattr_reader(sym)
+      class_eval(<<-EOS, __FILE__, __LINE__)
+        unless defined? @@#{sym}  # unless defined? @@hair_colors
+          @@#{sym} = nil          #   @@hair_colors = nil
+        end                       # end
+                                  #
+        def self.#{sym}           # def self.hair_colors
+          @@#{sym}                #   @@hair_colors
+        end                       # end
+                                  #
+        def #{sym}                # def hair_colors
+          @@#{sym}                #   @@hair_colors
+        end                       # end
+      EOS
     end
-  end
-  
-  def underscore
-    self.to_s.gsub(/::/, '/').
-      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-      gsub(/([a-z\d])([A-Z])/,'\1_\2').
-      tr("-", "_").
-      downcase
+
+    def cattr_writer(sym)
+      class_eval(<<-EOS, __FILE__, __LINE__)
+        unless defined? @@#{sym}                       # unless defined? @@hair_colors
+          @@#{sym} = nil                               #   @@hair_colors = nil
+        end                                            # end
+                                                       #
+        def self.#{sym}=(obj)                          # def self.hair_colors=(obj)
+          @@#{sym} = obj                               #   @@hair_colors = obj
+        end                                            # end
+      EOS
+    end
+
+    def cattr_accessor(sym)
+      cattr_reader(sym)
+      cattr_writer(sym)
+    end
+
   end
 
+  class String
+
+    def camelize(first_letter_in_uppercase = true)
+      if first_letter_in_uppercase
+        self.to_s.gsub(/\/(.?)/) { "::" + $1.upcase }.gsub(/(^|_)(.)/) { $2.upcase }
+      else
+        self.first + camelize(self)[1..-1]
+      end
+    end
+
+    def underscore
+      self.to_s.gsub(/::/, '/').
+        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+        gsub(/([a-z\d])([A-Z])/,'\1_\2').
+        tr("-", "_").
+        downcase
+    end
+
+  end
+    
 end
