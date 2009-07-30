864bea43b2c00bc9fd8447facac1351d7dfa2245
Jon Gilbraith <jon@professionalnerd.com>
2009-07-30 09:00:33 +0100

    Fixed bad implementation of core extensions.

--- lib/amazon_product_advertising_api.rb
+++ lib/amazon_product_advertising_api.rb
@@ -2,6 +2,10 @@ require 'net/http'
 require 'hpricot'
 
 require 'amazon_product_advertising_api/support'
+
+Class.send(:include, AmazonProductAdvertisingApi::CoreExtensions::Class)
+String.send(:include, AmazonProductAdvertisingApi::CoreExtensions::String)
+
 require 'amazon_product_advertising_api/base'
 require 'amazon_product_advertising_api/response_elements'
 require 'amazon_product_advertising_api/operations/base'
--- lib/amazon_product_advertising_api/support.rb
+++ lib/amazon_product_advertising_api/support.rb
@@ -1,67 +1,67 @@
 module AmazonProductAdvertisingApi #:nodoc:
-  
-  # Some extensions to the Class class.
-  class Class
+  module CoreExtensions
+    module Class
     
-    # Pleasant syntax for Class attribute readers.
-    def cattr_reader(sym)
-      class_eval(<<-EOS, __FILE__, __LINE__)
-        unless defined? @@#{sym}  # unless defined? @@hair_colors
-          @@#{sym} = nil          #   @@hair_colors = nil
-        end                       # end
-                                  #
-        def self.#{sym}           # def self.hair_colors
-          @@#{sym}                #   @@hair_colors
-        end                       # end
-                                  #
-        def #{sym}                # def hair_colors
-          @@#{sym}                #   @@hair_colors
-        end                       # end
-      EOS
-    end
-
-    # Pleasant syntax for Class attribute writers.
-    def cattr_writer(sym)
-      class_eval(<<-EOS, __FILE__, __LINE__)
-        unless defined? @@#{sym}                       # unless defined? @@hair_colors
-          @@#{sym} = nil                               #   @@hair_colors = nil
-        end                                            # end
-                                                       #
-        def self.#{sym}=(obj)                          # def self.hair_colors=(obj)
-          @@#{sym} = obj                               #   @@hair_colors = obj
-        end                                            # end
-      EOS
-    end
-
-    # Pleasant syntax for Class attribute accessors.
-    def cattr_accessor(sym)
-      cattr_reader(sym)
-      cattr_writer(sym)
-    end
-
-  end
-
-  # Some extensions to the String class.
-  class String
-
-    # Converts strings from under_score format to CamelCase
-    def camelize(first_letter_in_uppercase = true)
-      if first_letter_in_uppercase
-        self.to_s.gsub(/\/(.?)/) { "::" + $1.upcase }.gsub(/(^|_)(.)/) { $2.upcase }
-      else
-        self.first + camelize(self)[1..-1]
+      # Pleasant syntax for Class attribute readers.
+      def cattr_reader(sym)
+        class_eval(<<-EOS, __FILE__, __LINE__)
+          unless defined? @@#{sym}  # unless defined? @@hair_colors
+            @@#{sym} = nil          #   @@hair_colors = nil
+          end                       # end
+                                    #
+          def self.#{sym}           # def self.hair_colors
+            @@#{sym}                #   @@hair_colors
+          end                       # end
+                                    #
+          def #{sym}                # def hair_colors
+            @@#{sym}                #   @@hair_colors
+          end                       # end
+        EOS
       end
+
+      # Pleasant syntax for Class attribute writers.
+      def cattr_writer(sym)
+        class_eval(<<-EOS, __FILE__, __LINE__)
+          unless defined? @@#{sym}                       # unless defined? @@hair_colors
+            @@#{sym} = nil                               #   @@hair_colors = nil
+          end                                            # end
+                                                         #
+          def self.#{sym}=(obj)                          # def self.hair_colors=(obj)
+            @@#{sym} = obj                               #   @@hair_colors = obj
+          end                                            # end
+        EOS
+      end
+
+      # Pleasant syntax for Class attribute accessors.
+      def cattr_accessor(sym)
+        cattr_reader(sym)
+        cattr_writer(sym)
+      end
+
     end
 
-    # Converts strings from CamelCase format to under_score.
-    def underscore
-      self.to_s.gsub(/::/, '/').
-        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-        gsub(/([a-z\d])([A-Z])/,'\1_\2').
-        tr("-", "_").
-        downcase
-    end
+    # Some extensions to the String class.
+    module String
 
-  end
+      # Converts strings from under_score format to CamelCase
+      def camelize(first_letter_in_uppercase = true)
+        if first_letter_in_uppercase
+          self.to_s.gsub(/\/(.?)/) { "::" + $1.upcase }.gsub(/(^|_)(.)/) { $2.upcase }
+        else
+          self.first + camelize(self)[1..-1]
+        end
+      end
+
+      # Converts strings from CamelCase format to under_score.
+      def underscore
+        self.to_s.gsub(/::/, '/').
+          gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+          gsub(/([a-z\d])([A-Z])/,'\1_\2').
+          tr("-", "_").
+          downcase
+      end
+
+    end
     
+  end
 end
